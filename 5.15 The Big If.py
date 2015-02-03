c167beb698f31974ae73cb37f4ef5af3bc5ec727
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-02-02 22:53:00 -0600

    Create 5.15 The Big If.py

---
+++ 5.15 The Big If.py
@@ -0,0 +1,11 @@
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if 5 < 7:
+        return True
+        print "Nobody expects the Spanish Inquisition!"
+    elif 10 == 10 and 12 != 10:
+        return True
+        print "Right! Off you go!"
+    else:
+        return True
+        print "... and now for something completely different."
