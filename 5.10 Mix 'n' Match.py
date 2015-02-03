8c4f0bacf2b02b9c43e2ea4b602e096cddc0476f
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-02-02 22:50:55 -0600

    Create 5.10 Mix 'n' Match.py

---
+++ 5.10 Mix 'n' Match.py
@@ -0,0 +1,16 @@
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = not 2 > 3
+
+# Make me false!
+bool_three = 2 > 3 and 3 > 2
+
+# Make me true!
+bool_four = 2 > 3 or 3 > 2
+
+# Make me true!
+bool_five = not 2 > 3 and 3 > 2
