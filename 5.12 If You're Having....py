2dffca53822d70d4f9b5c88ec68877de06c8f379
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-02-02 22:51:37 -0600

    Create 5.12 If You're Having....py

---
+++ 5.12 If You're Having....py
@@ -0,0 +1,10 @@
+def using_control_once():
+    if 9>8:
+        return "Success #1"
+
+def using_control_again():
+    if 9==9:
+        return "Success #2"
+
+print using_control_once()
+print using_control_again()
